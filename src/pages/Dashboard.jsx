502e382109180833c5b0febfc608df202013cd40
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-03-27 13:01:15 +0530

    Added restricion with accessing dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { data, useNavigate } from "react-router-dom";
+import supabase from "../supabaseClient";
+import { toast } from "react-hot-toast";
 
-function Dashboard() {
+const Dashboard = () => {
+  const [user, setUser] = useState(null);
+  const[userName,setUserName] = useState(null);
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    navigate("/login");
+  }
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getUser = async () => {
+    try {
+      const { data:{user},error } = await supabase.auth.getUser() 
+      if(error){
+        toast.error(`Error fetching user:${error.message}`);
+        console.error(`Error fetching user:${error.message}`);
+        navigate("/login");
+        return
+      }
+      if (!user) {
+        toast.error("No user found");
+        console.error("No user found");
+        navigate("/login");
+        return
+      }
+      setUser(user);
+      setUserName(user.email.split("@")[0]);
+    
+    } catch (error) {
+      toast.error("Error fetching user:", error.message);
+      console.error("Error fetching user:", error.message);
+      navigate("/login");
+    }
+    }; 
+    getUser(); 
+  }, []);
+
+  if (!user) return <p>Loading...</p>;
+  // console.log( )
+
   return (
     <div>
-      User SignedUp
+      <h1>Welcome, {userName || "User"}!</h1>
+      <button onClick={handleLogout} className="bg-red-500 p-4">
+        Logout
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
