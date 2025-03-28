486a0890738a87b26a39165d3563f742aa3585a6
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-03-28 10:19:11 +0530

    remove duplicates

--- src/components/DoctorList.jsx
+++ src/components/DoctorList.jsx
@@ -22,7 +22,6 @@ const DoctorsList = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <h2 className="text-2xl font-bold mb-6 text-center">Available Doctors</h2>
 
       {/* Doctor Cards Grid */}
       <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-6">
