817ecefb21f3dbf3c1089517b7da62d3085fc03b
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-03-28 09:56:05 +0530

    Added dcotror list

--- src/components/DoctorList.jsx
+++ src/components/DoctorList.jsx
@@ -25,7 +25,7 @@ const DoctorsList = () => {
       <h2 className="text-2xl font-bold mb-6 text-center">Available Doctors</h2>
 
       {/* Doctor Cards Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-6">
         {doctors.map((doctor) => (
           <div
             key={doctor.id}
